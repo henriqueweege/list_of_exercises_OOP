1ea879e8555259f4d2d44596ced6d2d22d170fc0
henriqueweege <henriqueweege@gmail.com>
2022-04-12 15:03:12 -0300

    Start of exercise 5.

---
+++ List1/Exercise5/Exercise5.cs
@@ -0,0 +1,25 @@
+﻿
+
+using ClassExercise5;
+
+public class Exercise5
+{
+
+    public static void Main(string[] args)
+    {
+        
+        var phrase = "this is one phrase";
+        var manipulator = new StringManipulator();
+
+        foreach(var word in manipulator.TakeSpacesOff(phrase))
+        {
+            Console.Write(word +" ");
+            Console.WriteLine(word.Length);
+
+        }
+
+        
+        Console.ReadLine();
+
+    }
+}
