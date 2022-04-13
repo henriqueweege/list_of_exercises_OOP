bc0e7a2bcd8b7668a54e918e9eb27ed9bbf851d9
henriqueweege <henriqueweege@gmail.com>
2022-04-13 08:42:47 -0300

    Exercise 6
    Still need to do tests.

---
+++ List1/ArithimeticExpresisonsVerifications/ArithimeticExpresisonsVerifications.cs
@@ -0,0 +1,58 @@
+﻿namespace Library
+{
+    public class ArithimeticExpresisonsVerifications
+    {
+        public string ParentesisVerify(string EXPRESSION)
+        {
+            var expression = EXPRESSION;
+            var parentesis = new List<char>();
+            foreach(var character in expression)
+            {
+                if (character == ')' && parentesis.Count == 0)
+                {
+                    return "The parentesis aren't right";
+                }
+                if (character == '(')
+                {
+                    parentesis.Add(character);
+                }
+                else if (character == ')')
+                {
+                    parentesis.Remove(parentesis[0]);
+                }
+            }
+            return "The parentesis are right";
+        }
+
+        public string DivisionAndMultiplicationCounter(string EXPRESSION)
+        {
+            var division = 0;
+            var multiplication = 0;
+            foreach(var character in EXPRESSION)
+            {
+                if (character == '*')
+                {
+                    multiplication++;
+                }
+                if (character == '/')
+                {
+                    division++;
+                }
+            }
+            return $"The number of Division in the expression if {division}, and the number of multiplication is {multiplication}";
+        }
+
+        public string PositionAndTypeOfFirstOperator(string EXPRESSION)
+        {
+            var positionOfOperator = new List<int>(){
+                EXPRESSION.IndexOf("+"),
+                EXPRESSION.IndexOf("-"),
+                EXPRESSION.IndexOf("*"),
+                EXPRESSION.IndexOf("/")
+                };
+            positionOfOperator.Sort();
+            return $"The first operator is {EXPRESSION[positionOfOperator[0]]}, and it is in the position {positionOfOperator[0]}";
+
+        }
+    }
+}
---
+++ List1/Exercise6/Exercise6.cs
@@ -0,0 +1,17 @@
+﻿using Library;
+
+public class Exercise6
+{
+    public static void Main(string [] args)
+    {
+        var expressionCheck = new ArithimeticExpresisonsVerifications();
+        var arithimeticExpression = "(a + 10) / (23 * (10.5 - b) - 2.59 / (b * a))";
+        var isParentesisRight = expressionCheck.ParentesisVerify(arithimeticExpression);
+        var quantityOfDivisionsAndMultiplications = expressionCheck.DivisionAndMultiplicationCounter(arithimeticExpression);
+        var positionAndTypeOfFirstOperator = expressionCheck.PositionAndTypeOfFirstOperator(arithimeticExpression);
+
+        Console.WriteLine(isParentesisRight);
+        Console.WriteLine(quantityOfDivisionsAndMultiplications);
+        Console.WriteLine(positionAndTypeOfFirstOperator);
+    }
+}
